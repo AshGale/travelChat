84e20ba92b728857803ae350653c0d1efa9347d9
AshGale <ashgale17@yahoo.co.uk>
2019-05-11 17:12:22 +0100

    Initial Commit

---
+++ src/main/java/co/uk/travelChat/MongoReactiveApplication.java
@@ -0,0 +1,17 @@
+package co.uk.travelChat;
+
+//@EnableReactiveMongoRepositories
+//public class MongoReactiveApplication
+//        extends AbstractReactiveMongoConfiguration {
+//
+//    @Override
+//    protected String getDatabaseName() {
+//        return "reactive";
+//    }
+//
+//    @Override
+//    public MongoClient reactiveMongoClient() {
+//        return MongoClients.create();
+//    }
+//}
+
---
+++ src/main/java/co/uk/travelChat/TravelChatApplication.java
@@ -0,0 +1,13 @@
+package co.uk.travelChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelChatApplication.class, args);
+	}
+
+}

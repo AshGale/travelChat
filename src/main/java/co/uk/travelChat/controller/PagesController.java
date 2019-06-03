eae320b82e583dfff32e9e214771fcb1be37c3ba
AshGale <ashgale17@yahoo.co.uk>
2019-06-03 20:30:29 +0100

    added Thymleaf to the project

---
+++ src/main/java/co/uk/travelChat/controller/PagesController.java
@@ -0,0 +1,20 @@
+package co.uk.travelChat.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PagesController {
+
+    //this loads the index page from temples when / is called
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+//    @GetMapping("/login")
+//    public String login() {
+//        return "login";
+//    }
+}

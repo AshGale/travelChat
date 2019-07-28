529ee6e31feb86a3547baaf419be6eb1a6a8b180
AshGale <ashgale17@yahoo.co.uk>
2019-07-28 14:58:26 +0100

    Added Embedded Mongo, and set up tests  - Account Crud and Service  - doing Controller

--- src/test/java/co/uk/travelChat/controller/AccountControllerTest.java
+++ src/test/java/co/uk/travelChat/controller/AccountControllerTest.java
@@ -1,18 +1,20 @@
 package co.uk.travelChat.controller;
 
+import co.uk.travelChat.TravelChatApplication;
 import co.uk.travelChat.model.Account;
 import co.uk.travelChat.repository.AccountCrudRepository;
 import co.uk.travelChat.service.AccountService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
@@ -20,42 +22,61 @@ import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(classes = TravelChatApplication.class)
+@DataMongoTest(includeFilters = {@ComponentScan.Filter(Service.class), @ComponentScan.Filter(Controller.class)})
 public class AccountControllerTest {
 
     private static final String defaultId = "507f1f77bcf86cd799439011";
 
-    private static Account account = new Account(null, "account", "account", new ArrayList<>());
+    private static Account testAccount = new Account(null, "account", "account", new ArrayList<>());
 
     private static Account savedAccount = new Account(defaultId, "account", "account", new ArrayList<>());
 
-    @Mock
+    @Autowired
     private AccountCrudRepository accountCrudRepository;
 
-    @Mock
+    @Autowired
     private AccountService accountService;
 
-    @InjectMocks
+    @Autowired
     private AccountController underTest;
 
-    @Autowired
     private WebTestClient webTestClient;
 
     @Before
     public void init() {
-        MockitoAnnotations.initMocks(this);
         this.webTestClient = WebTestClient.bindToController(new AccountController(accountService)).build();
+    }
+
+    @Test
+    public void getAccountById() {
+
+        accountCrudRepository.save(savedAccount).subscribe();
+
+        webTestClient.get().uri("/account/" + defaultId)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .expectBody(Account.class)
+                .value(account -> {
+                    assertEquals(savedAccount.getId(), account.getId());
+                    assertEquals(savedAccount.getName(), account.getName());
+                    assertEquals(savedAccount.getNickname(), account.getNickname());
+                    assertEquals(savedAccount.getTrips(), account.getTrips());
+                });
     }
 
     @Test
     public void saveAccount() {
 
-        Mockito.when(accountService.saveAccount(account))
+        Mockito.when(accountService.saveAccount(testAccount))
                 .thenReturn(Mono.just(savedAccount));
 
         webTestClient.post().uri("/account")
-                .body(Mono.just(account), Account.class)
+                .body(Mono.just(testAccount), Account.class)
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody(Account.class)
@@ -72,21 +93,6 @@ public class AccountControllerTest {
                 .exchange()
                 .expectBody()
                 .isEmpty();
-
-    }
-
-    @Test
-    public void getAccountById() {
-
-        Mockito.when(accountService.getAccountById(defaultId))
-                .thenReturn(Mono.just(savedAccount));
-
-        webTestClient.get().uri("/account/" + defaultId)
-                .exchange()
-                .expectStatus().isOk()
-                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
-                .expectBody(Account.class)
-                .isEqualTo(savedAccount);
 
     }
 
--- src/test/java/co/uk/travelChat/repository/AccountCrudRepositoryManualTest.java
+++ src/test/java/co/uk/travelChat/repository/AccountCrudRepositoryManualTest.java
@@ -1,11 +1,10 @@
 package co.uk.travelChat.repository;
 
-import co.uk.travelChat.TravelChatApplication;
 import co.uk.travelChat.model.Account;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -16,22 +15,22 @@ import java.util.ArrayList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+@DataMongoTest
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = TravelChatApplication.class)
 public class AccountCrudRepositoryManualTest {
 
     @Autowired
     AccountCrudRepository repository;
 
     @Test
-    public void givenNickname_whenFindAllByNickname_thenFindAccount() {
-        repository.save(new Account(null, "Bill", "bil", new ArrayList<>())).block();
+    public void findFirst1ByNicknameIgnoreCase() {
+        repository.save(new Account(null, "Bill", "bil", new ArrayList<>())).subscribe();
         Mono<Account> accountFlux = repository.findFirst1ByNicknameIgnoreCase("bil");
 
         StepVerifier.create(accountFlux)
                 .assertNext(account -> {
                     assertEquals("Bill", account.getName());
-                    assertEquals("bil" , account.getNickname());
+                    assertEquals("bil", account.getNickname());
                     assertNotNull(account.getId());
                 })
                 .expectComplete()
@@ -39,25 +38,22 @@ public class AccountCrudRepositoryManualTest {
     }
 
     @Test
-    public void givenName_whenFindFirstByName_thenFindAccount() {
-        repository.save(new Account(null, "Bill", "bil", new ArrayList<>())).block();
+    public void findAccountsByName() {
+        repository.save(new Account(null, "Bill", "bil", new ArrayList<>())).subscribe();
         Flux<Account> accountMono = repository.findAccountsByName("Bill");
 
         StepVerifier.create(accountMono)
                 .assertNext(account -> {
                     assertEquals("Bill", account.getName());
-                    assertEquals("bil" , account.getNickname());
+                    assertEquals("bil", account.getNickname());
                     assertNotNull(account.getId());
                 })
                 .expectComplete()
                 .verify();
-
-
-
     }
 
     @Test
-    public void givenAccount_whenSave_thenSaveAccount() {
+    public void AccountSave() {
         Mono<Account> accountMono = repository.save(new Account(null, "Bill", "bil", new ArrayList<>()));
 
         StepVerifier

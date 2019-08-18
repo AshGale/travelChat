cbbc3d0c64bda2553be7022dd4ac8682e634139b
AshGale <ashgale17@yahoo.co.uk>
2019-08-18 11:19:52 +0100

    - fixed minor bugs

--- src/main/resources/static/js/editor.js
+++ src/main/resources/static/js/editor.js
@@ -5,7 +5,11 @@ window.addEventListener("load", function () {
         event.preventDefault();
 
         let account = getAccountForm();
-        post_json('/account', account);
+        if(account == null) {
+            console.log("Request not send due to error");
+        } else {
+            post_json('/account', account);
+        }
     });
 
     $('#get-account').click(function(event) {
@@ -18,42 +22,46 @@ window.addEventListener("load", function () {
             document.getElementById("account_id").value = "";
             document.getElementById("name-input").value = "";
             document.getElementById("nickname-input").value = "";
+            document.getElementById("trips-input").value = "";
     });
  });
 
 async function processRequesFortAccount(){
     let result;
     let form_account = getAccountForm();//get account fields from form
-
-    //determine type of get to perform, and wait for server response
-    if(form_account.id != null && form_account.id != "") {
-        result = await get_json('/account/' + form_account.id);
-    } else if(form_account.nickname != "" && form_account.nickname != null) {
-        result = await get_json('/account/nickname/' + form_account.nickname);
-    } else if(form_account.name != "" && form_account.name != null) {
-         result = await get_json('/account/name/' + form_account.name);//encodeURIComponent(name)
+    if(form_account == null) {
+        console.log("Request not send due to error");
     } else {
-        alert("No identifiable information provided\nPlease enter an Id, Nickname, or Name");
-    }
+        //determine type of get to perform, and wait for server response
+        if(form_account.id != null && form_account.id != "") {
+            result = await get_json('/account/' + form_account.id);
+        } else if(form_account.nickname != "" && form_account.nickname != null) {
+            result = await get_json('/account/nickname/' + form_account.nickname);
+        } else if(form_account.name != "" && form_account.name != null) {
+             result = await get_json('/account/name/' + form_account.name);//encodeURIComponent(name)
+        } else {
+            alert("No identifiable information provided\nPlease enter an Id, Nickname, or Name");
+        }
 
-    //process the result.
-    //if error ignore
-    //if object of single account, fill form
-    //if array, show number of accounts, and store result, and add number selection
+        //process the result.
+        //if error ignore
+        //if object of single account, fill form
+        //if array, show number of accounts, and store result, and add number selection
 
-    if(result.constructor === Account){
-        console.log("of type account");
-    }
-    else if(result.constructor === Array) {
-        console.log("of type Array " + result.length);
-    }
-    else if(result.constructor === Object) {
-        console.log("of type Object");
-    } else {console.log("of type " + result.constructor );}
+        if(result.constructor === Account){
+            console.log("of type account");
+        }
+        else if(result.constructor === Array) {
+            console.log("of type Array " + result.length);
+        }
+        else if(result.constructor === Object) {
+            console.log("of type Object");
+        } else {console.log("of type " + result.constructor );}
 
-    let account = getAccountObjectFromResult(result);
+        let account = getAccountObjectFromResult(result);
 
-    populateAccountForm(account);
+        populateAccountForm(account);
+     }
  }
 
 function getAccountObjectFromResult(result) {
@@ -81,7 +89,17 @@ function populateAccountForm(account) {
     }
     account.name = $('#name-input').val();
     account.nickname = $('#nickname-input').val();
-    account.trips = JSON.parse($('#trips-input').val());//["5d5827bb9660491f480fad3d"]
+    if($('#trips-input').val() == "" || $('#trips-input').val() == null) { // ensure null is sent when nothing is filled
+        account.trips = null;
+    } else {
+        try {
+            account.trips = JSON.parse($('#trips-input').val());//["5d5827bb9660491f480fad3d"]
+        } catch {
+            window.alert("Please ensure trips are in format [\"value\",\"value\"]");
+            return null;
+        }
+    }
+
 
     return account;
  }
